c4850fd1a0833e10cb8b00bfa564069649d9c681
Felix Boerner <ich@felix-boerner.de>
2025-06-10 08:17:12 +0200

    feat: moved cmdline.txt to boot/firmware
    BREAKING CHANGE

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -3,7 +3,7 @@
 
 def test_cmdline_file(host):
     """Check Linux kernel command line file"""
-    f = host.file("/boot/cmdline.txt")
+    f = host.file("/boot/firmware/cmdline.txt")
 
     assert f.is_file
     assert f.user == "root"
@@ -12,9 +12,8 @@ def test_cmdline_file(host):
     assert (
         "console=serial0,115200 "
         "console=tty1 "
-        "root=UUID=49a6dc8a-761b-418a-bb46-344127dcc1a0 "
+        "root=PARTUUID=ebbeb7e0-01 "
         "rootfstype=ext4 "
-        "elevator=deadline "
         "fsck.repair=yes "
         "rootwait"
     ) in f.content_string
